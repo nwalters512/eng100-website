288dc0b432fcc18694c7d8af97acf98c20ee094f
Nathan Walters <nwalters512@gmail.com>
2016-04-05 17:26:42 -0500

    moved title and description to blocks for extensibility

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'njk');
 nunjucks.configure('views', {
   autoescape: true,
+  trimBlocks: true,
   express: app
 });
 
