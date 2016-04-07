1baded08b748acacf96e071f1363f4019ef066ff
Nathan Walters <nwalters512@gmail.com>
2016-04-07 00:53:07 -0500

    Move livereload to run on dev environment only

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('connect-livereload')());
+if (app.get('env') === 'development') {
+  app.use(require('connect-livereload')());
+}
 
 app.set('view engine', 'njk');
 
